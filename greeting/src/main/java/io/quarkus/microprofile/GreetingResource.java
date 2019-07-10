70b094e412f0ea6762907c458916b0912f560c4a
Ken Finnigan <ken@kenfinnigan.me>
2019-07-10 11:15:23 -0400

    Initial project

---
+++ greeting/src/main/java/io/quarkus/microprofile/GreetingResource.java
@@ -0,0 +1,29 @@
+package io.quarkus.microprofile;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class GreetingResource {
+
+    @GET
+    @Path("/greeting")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Greeting greeting() {
+        return new Greeting("Hello World!");
+    }
+
+    public class Greeting {
+        private String greeting;
+
+        public Greeting(String greeting) {
+            this.greeting = greeting;
+        }
+
+        public String getGreeting() {
+            return greeting;
+        }
+    }
+}
---
+++ name/src/main/java/io/quarkus/microprofile/NameResource.java
@@ -0,0 +1,17 @@
+package io.quarkus.microprofile;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class NameResource {
+
+    @GET
+    @Path("/name")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Name name() {
+        return new Name("Nobody");
+    }
+}
